b20adb8331764925020618ef93c1a6c47b88fe55
dreevo <sidedark85@gmail.com>
2024-09-24 14:52:35 +0100

    first commit

---
+++ src/main/java/com/bookshop/bookshopcatalogservice/web/BookController.java
@@ -0,0 +1,44 @@
+package com.bookshop.bookshopcatalogservice.web;
+
+import com.bookshop.bookshopcatalogservice.domain.Book;
+import com.bookshop.bookshopcatalogservice.domain.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public Iterable<Book> get() {
+        return bookService.viewBookList();
+    }
+
+    @GetMapping("{isbn}")
+    public Book getByIsbn(@PathVariable String isbn) {
+        return bookService.viewBookDetails(isbn);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book post(@Valid @RequestBody Book book) {
+        return bookService.addBookToCatalog(book);
+    }
+
+    @DeleteMapping("{isbn}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String isbn) {
+        bookService.removeBookFromCatalog(isbn);
+    }
+
+    @PutMapping("{isbn}")
+    public Book put(@Valid @PathVariable String isbn, @RequestBody Book book) {
+        return bookService.editBookDetails(isbn, book);
+    }
+}
